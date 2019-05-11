de237171baaac09b14e68fa55cd511cb6299fd4f
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-11 18:17:50 -0400

    Updates

---
+++ precache-manifest.572bae98af6890e361af91d5d5dab6f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc8e1aac4fc4b0d7fc684bbb023a553",
+    "url": "/cutest-dogs-ever-react-app/index.html"
+  },
+  {
+    "revision": "0f6e4fc8dd927e1723e0",
+    "url": "/cutest-dogs-ever-react-app/static/css/main.f661f3d2.chunk.css"
+  },
+  {
+    "revision": "28c56f31ecaca88c4f9a",
+    "url": "/cutest-dogs-ever-react-app/static/js/2.28d4ca51.chunk.js"
+  },
+  {
+    "revision": "0f6e4fc8dd927e1723e0",
+    "url": "/cutest-dogs-ever-react-app/static/js/main.87e594ed.chunk.js"
+  },
+  {
+    "revision": "da7be3d338a657d4a8ca",
+    "url": "/cutest-dogs-ever-react-app/static/js/runtime~main.4ca2ea0d.js"
+  }
+]);
